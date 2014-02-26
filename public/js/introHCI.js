ba01b2237adeb601ae500a5f6bcf99009d28f8a7
Ted <tedkim@stanford.edu>
2014-02-26 22:52:48 +0000

    adding user timing for first click

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var startTime;
+var eventSent;	// so we only track timing for first click
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -10,7 +13,18 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	startTime = new Date().getTime();
+	eventSent = false;
+
 	$(".likeBtn").click(function () {
 	  ga("send", "event", "like", "click");
+
+	  if (!eventSent) {
+	  	var timeSpent = endTime - startTime;
+	  	ga('send', 'timing', 'Like', 'First Like', timeSpent);
+	  	eventSent = true;
+	  }
 	});
+
+
 }
